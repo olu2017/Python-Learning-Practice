d40c4d3d6ffce5409ab092e9fbb680d54817706a
“Olu <olulakun@gmail.com>
2026-04-26 20:45:00 +0100

    Updated

--- KnowledgeSheet.py
+++ KnowledgeSheet.py
@@ -1,3 +1,17 @@
 #Print Function to display something on the screen
 print("Hello World")
 
+'''
+Multi comment line in python
+Another Line
+
+Empty Variable in python is None
+'''
+
+#Variable in python should use snake_case and be descriptive
+age = 25
+first_name = "David"
+
+print(f"Candidates name is {first_name} and age is {age}")
+print(f"Candidates name is {first_name} and age is {age}")
+
